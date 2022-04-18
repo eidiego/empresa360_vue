eaa8eddefb01212b880b93e78d38c383cc344fde
Diego Rodrigues <diegokod@gmail.com>
2022-04-17 22:10:31 -0300

    Add: Component Home

--- src/views/MyHome.vue
+++ src/views/MyHome.vue
@@ -1,8 +1,54 @@
 <template>
     <div>
-        <h3>Home</h3>
-       
-        <router-link to="/"> Sair </router-link>
+        <nav class="navbar navbar-light bg-light menu-superior">
+            <div class="container">
+                <a class="navbar-brand" href="#">Empresa 360</a>
+                <div class="navbar-nav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/">Sair</router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="d-flex">
+            <div class="list-group menu-esquerda">
+                <a class="list-group-item list-group-item-action" href="#"><i class="bi bi-speedometer2"></i> Dashboard</a>
+            </div>
+
+            <div class="w-100">
+                <nav aria-label="breadcrumb" class="pt-1 ps-3">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                    </ol>
+                </nav>
+
+                <div class="container-fluid">
+                    <h1 class="mt-4">Visões relacionadas ao menu</h1>
+                    <p>Aqui podemos apresentar as views de acordo com as opções do menu.</p>
+                </div>
+
+            </div>
+        </div>
     </div>
-   
 </template>
+
+
+<style scoped>
+.menu-superior {
+    border-bottom: solid 1px #ccc;
+}
+
+.menu-esquerda {
+    border-right: solid 1px #ccc;
+    width: 250px;
+}
+
+.list-group-item {
+    border-top: none;
+    border-right: none;
+}
+
+</style>
