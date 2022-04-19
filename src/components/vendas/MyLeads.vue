cfb009da891ee716b8ebfe9bf9f0877bd5465c9f
Diego Rodrigues <diegokod@gmail.com>
2022-04-18 22:44:16 -0300

    Feature: Adicionado css na tabela Leads

--- src/components/vendas/MyLeads.vue
+++ src/components/vendas/MyLeads.vue
@@ -1,8 +1,29 @@
 <template>
-    <h3> Leads </h3>
-
-    {{ dados }}
-
+    <div>
+        <h3> Leads </h3>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">NOME</th>
+                    <th scope="col">TELEFONE</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="d in dados" :key="d.id">
+                    <td> {{ d.id }} </td>
+                    <td> {{ d.nome }} </td>
+                    <td> {{ d.telefone }} </td>
+                    <td>
+                       <router-link to="/home/vendas/leads" class="btn btn-sm btn-primary"> 
+                            <i class="bi bi-pencil-square" style="cursor: pointer;"></i>
+                       </router-link>
+                    </td>
+                </tr>
+            </tbody>    
+        </table>
+    </div>
 </template>
 
 <script>
