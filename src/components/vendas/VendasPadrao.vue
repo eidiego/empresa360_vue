b26db0ee0005810d6a3f56622cf3622311773856
Diego Rodrigues Barbosa <dbarbosa@clubecirculo.com>
2022-04-19 13:05:13 -0300

    Add: Componente VendasPadrão e rotas

---
+++ src/components/vendas/VendasPadrao.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-3 offset-2 bg-suc">
+                <div class="card">
+                    <div class="card-header bg-success text-white">Leads</div>
+                    <div class="card-body">1500</div>
+                </div>
+            </div>
+
+            <div class="col-3 offset-2">
+                <div class="card">
+                    <div class="card-header  bg-success text-white">Contratos</div>
+                    <div class="card-body">250</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
--- src/router.js
+++ src/router.js
@@ -4,25 +4,29 @@ import Leads from '@/components/vendas/MyLeads.vue'
 import Lead from '@/components/vendas/MyLead.vue'
 import Dashboard from '@/components/dashboard/MyDashboard.vue'
 import Contratos from '@/components/vendas/MyContracts.vue'
+import VendasPadrao from '@/components/vendas/VendasPadrao.vue'
 import Servicos from '@/components/servicos/MyServices.vue'
 import Home from '@/views/MyHome.vue'
 import Login from '@/views/MyLogin.vue'
 import Site from '@/views/MySite.vue'
 
-const routes = [
-    {
+const routes = [{
         path: '/', //localhost:8080/
         component: Site
     },
     {
         path: '/home', //localhost:8080/home
         component: Home,
-        children: [
-            { path: 'vendas', component: Vendas, children: [
-                {path: 'leads', component: Leads},
-                {path: 'leads/:id', component: Lead},
-                {path: 'contratos', component: Contratos},
-            ]}, //localhost:8080/home/vendas
+        children: [{
+                path: 'vendas',
+                component: Vendas,
+                children: [
+                    { path: 'leads', component: Leads },
+                    { path: 'leads/:id', component: Lead },
+                    { path: 'contratos', component: Contratos },
+                    { path: '', component: VendasPadrao },
+                ]
+            }, //localhost:8080/home/vendas
             { path: 'servicos', component: Servicos }, //localhost:8080/home/servicos
             { path: 'dashboard', component: Dashboard }
         ]
@@ -31,7 +35,7 @@ const routes = [
         path: '/login', //localhost:8080/login
         component: Login
     }
-] 
+]
 
 const router = createRouter({
     history: createWebHistory(),
