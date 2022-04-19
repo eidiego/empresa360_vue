637e6798cae25103ab824470881f7d62eea8b50a
Diego Rodrigues <diegokod@gmail.com>
2022-04-18 22:34:21 -0300

    Add: API com dados de leads

--- src/components/vendas/MyLeads.vue
+++ src/components/vendas/MyLeads.vue
@@ -1,3 +1,27 @@
 <template>
     <h3> Leads </h3>
+
+    {{ dados }}
+
 </template>
+
+<script>
+export default {
+    name: 'MyLeads',
+    data: () => ({
+        dados: null
+    }),
+    methods: {
+        getDadosApi() {
+            fetch('http://localhost:3000/leads')
+                .then(response => response.json())
+                .then(response => {
+                    this.dados = response
+                })
+        }
+    },
+      created() {
+        this.getDadosApi()
+    }   
+}
+</script>
