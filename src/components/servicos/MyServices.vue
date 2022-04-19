3f14903a830a8cb73e9b70041a58bf85205dd933
Diego Rodrigues Barbosa <dbarbosa@clubecirculo.com>
2022-04-19 18:42:15 -0300

    Add: Component MyServices and new endpoint json

--- src/components/servicos/MyServices.vue
+++ src/components/servicos/MyServices.vue
@@ -1,3 +1,28 @@
 <template>
-    <h5>Servicos</h5>
+    <div>
+        <h3> Serviços </h3>
+        <div class="row">
+            <div class="col" v-for="d in dados" :key="d.id">
+                <div class="card" style="width: 11rem">
+                    <img class="card-img-top" :src="`/img/${d.icone}`">
+                    <div class="card-body text-center" >
+                        <p class="card-text">
+                            {{d.servico}}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+import ApiMixin from "@/mixins/ApiMixin"
+export default {
+    name: 'MyServices',
+    mixins: [ApiMixin],
+      created() {
+        this.getDadosApi('http://localhost:3000/servicos')
+    }   
+}
+</script>
