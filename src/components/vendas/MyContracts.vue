3853450ee5a7f6626f54d1bce8dcea2a41a21833
Diego Rodrigues Barbosa <diegokod@gmail.com>
2022-04-20 19:06:05 -0300

    Add: Component MyContracts

--- src/components/vendas/MyContracts.vue
+++ src/components/vendas/MyContracts.vue
@@ -1,3 +1,37 @@
 <template>
-    <h3> Contratos </h3>
+    <div>
+        <h3> Contratos </h3>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">LEAD</th>
+                    <th scope="col">SERVICO</th>
+                    <th scope="col">DATA INICIO</th>
+                    <th scope="col">DATA FIM</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="d in dados" :key="d.id">
+                    <td> {{ d.id }} </td>
+                    <td> {{ d.leadId }} </td>
+                    <td> {{ d.servicoId }} </td>
+                    <td> {{ d.data_inicio }} </td>
+                    <td> {{ d.data_fim }} </td>
+                
+                </tr>
+            </tbody>    
+        </table>
+    </div>
 </template>
+
+<script>
+import ApiMixin from "@/mixins/ApiMixin"
+export default {
+    name: 'MyContracts',
+    mixins: [ApiMixin],
+      created() {
+        this.getDadosApi('http://localhost:3000/contratos')
+    }   
+}
+</script>
